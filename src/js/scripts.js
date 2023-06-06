c4bd463afbbe392b47b0893132fcd1627bc95ef9
rcastillod <rodrigo.castillo@global3.cl>
2023-06-06 09:23:32 -0400

    fix: :bug: Fix the toggle addEventListener TypeError

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,7 +2,9 @@
 const toggle = document.querySelector('.products__toggle')
 const productList = document.querySelector('.sidebar__products ul')
 
-toggle.addEventListener('click', () => {
-  toggle.classList.toggle('active')
-  productList.classList.toggle('show')
-})
+if (toggle) {
+  toggle.addEventListener('click', () => {
+    toggle.classList.toggle('active')
+    productList.classList.toggle('show')
+  })
+}
